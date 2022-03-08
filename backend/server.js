b4da0c757a07b84c84887d8c47145ed7fda4b09f
Vinaga Petru <vinagapetru11@gmail.com>
2022-03-08 23:11:18 +0200

    Creat express server pentru a trimite date din backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import data from "./data.js";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/api/products', (req,res) => {
+    res.send(data.products);
+});
+
+app.listen(port, () => {
+    console.log(`Server is start in port ${port}`);
+});
+
+
+
