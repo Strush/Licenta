c388aa6232381e781f1b6f7f2940e278e1e8f27c
Vinaga Petru <vinagapetru11@gmail.com>
2022-05-22 18:27:07 +0300

    Sa restaurat versiunea de productie

--- backend/server.js
+++ backend/server.js
@@ -39,11 +39,11 @@ app.use('/api/keys/paypal',(req,res) => {
 });
 
 /* */
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
-// app.get('*', (req,res) => {
-//     res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
-// });
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
 
 // ExpressAsyncHandler afisarea erorilor 
 app.use((err,req,res,next) => {
