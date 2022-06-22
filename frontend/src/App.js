17da97df901f7c5a56df3b27159b67b3c3e2b126
Vinaga Petru <vinagapetru11@gmail.com>
2022-06-23 01:41:55 +0300

    Fix version of package npm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Helmet } from 'react-helmet-async';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
