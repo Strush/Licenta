1e0f9b45c2433ca501b5b42357ea8d23f82b80bc
Vinaga Petru <vinagapetru11@gmail.com>
2022-06-23 01:50:07 +0300

    Sa creat versiunea de mobil pentru pagina comanda, si sa daugat imagini in pasi.

--- frontend/src/components/FinishSteps.jsx
+++ frontend/src/components/FinishSteps.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBoxOpen,faHomeUser,faCircleDollarToSlot, faMapLocationDot} from '@fortawesome/free-solid-svg-icons';
 
 export default function FinishSteps(props) {
   return (
     <Container>
-      <Row className='finish-steps mb-4 mb-sm-5'>
-        <Col className={props.step1 ? 'active' : ''}>Autentificare</Col>
-        <Col className={props.step2 ? 'active' : ''}>Livrare</Col>
-        <Col className={props.step3 ? 'active' : ''}>Achită</Col>
-        <Col className={props.step4 ? 'active' : ''}>Plasează Comanda</Col>
+      <Row className='finish-steps mb-4'>
+        <Col className={props.step1 ? 'active' : ''}><FontAwesomeIcon icon={faHomeUser} /></Col>
+        <Col className={props.step2 ? 'active' : ''}><FontAwesomeIcon icon={faMapLocationDot} /></Col>
+        <Col className={props.step3 ? 'active' : ''}><FontAwesomeIcon icon={faCircleDollarToSlot} /></Col>
+        <Col className={props.step4 ? 'active' : ''}><FontAwesomeIcon icon={faBoxOpen} /></Col>
       </Row>
     </Container>
   )
